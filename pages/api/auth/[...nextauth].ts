4116bad7172abb02e8049be6d81590951b19760c
LeeChanHyeok <leechanhyeok@LeeChanHyeokui-MacBookPro.local>
2023-06-11 01:44:55 +0900

    1:44

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -9,8 +9,8 @@ import { AuthOptions } from "next-auth";
 export const authOptions: AuthOptions = {
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET_ID ?? "",
+      clientId: process.env.GITHUB_CLIENT_ID || "",
+      clientSecret: process.env.GITHUB_SECRET_ID || "",
     }),
     CredentialsProvider({
       //1. 로그인페이지 폼 자동생성해주는 코드
